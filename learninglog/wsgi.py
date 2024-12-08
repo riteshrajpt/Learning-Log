fff221eba43f23f50ddcc5a905ae9d6dbe0e8f3b
Ritesh Singh <72240431+riteshrajpt@users.noreply.github.com>
2024-12-09 01:26:43 +0530

    Update wsgi.py

--- learninglog/wsgi.py
+++ learninglog/wsgi.py
@@ -14,4 +14,6 @@ from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learninglog.settings')
 application = Cling(get_wsgi_application())
+# api/wsgi.py
+app = get_wsgi_application()
 
